1277adc5f67b0941aed49531ed18b7da57aea3c0
BeibaRoman <106346781+BeibaRoman@users.noreply.github.com>
2026-02-07 15:54:46 +0000

    Deploying to gh-pages from @ BeibaRoman/goit-js-hw-09@619e8ff00f69b01839c53f27531b4cedee9528e7 🚀

---
+++ 01-color-switcher.72764263.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")};let n=null;t.stopBtn.disabled=!0,t.startBtn.addEventListener("click",(function(){if(n)return;t.startBtn.disabled=!0,t.stopBtn.disabled=!1,n=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),t.stopBtn.addEventListener("click",(function(){t.startBtn.disabled=!1,t.stopBtn.disabled=!0,clearInterval(n),n=null}));
+//# sourceMappingURL=01-color-switcher.72764263.js.map
